6482c0392d54730197fb70bd588bc433679f1491
tenirina <virhot17@gmail.com>
2022-11-26 11:34:36 +0600

    Add
    Добавлена модель Image, так же добавлена даннная модель в админку. Сделаны и применены миграции.

--- app/webapp/admin.py
+++ app/webapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from webapp.models import Image
+
+
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'image', 'text', 'author', 'created_at')
+    list_filter = ('id', 'text', 'author')
+    search_fields = ('id', 'text', )
+    fields = ('id', 'image', 'text', 'author', 'favorites', 'created_at')
+    readonly_fields = ('id', 'author')
+
+
+admin.site.register(Image, ImageAdmin)
---
+++ app/webapp/models/images.py
@@ -0,0 +1,15 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+
+class Image(models.Model):
+    image = models.ImageField(verbose_name='Photo', null=False, blank=False, upload_to='images')
+    text = models.CharField(verbose_name='Text', null=False, blank=False, max_length=100)
+    author = models.ForeignKey(verbose_name='Author', to=get_user_model(), related_name='images', null=False, blank=False,
+                               on_delete=models.CASCADE)
+    created_at = models.DateTimeField(verbose_name='Date of creation', auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='Date of change', auto_now=True)
+    favorites = models.ManyToManyField(to=get_user_model(), related_name='users', blank=True)
+
+    def __str__(self):
+        return f'{self.author} : {self.text[:30]}'
